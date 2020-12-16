40563509a6f12b3f33c394cedd4e9d25a39c8c12
Kostya <konstantin.sedniov@gmail.com>
2020-12-16 03:06:39 +0300

    added main

--- Hooker/Source.cpp
+++ Hooker/Source.cpp
@@ -1,8 +1,35 @@
-#include <Windows.h>
-#include <iostream>
-#include <string> 
+#include <cstdio>
+#include <windows.h>
+#include <detours.h>
 
-int main()
+TCHAR dllName[] = TEXT("HookerDll");
+
+int main(int argc, wchar_t* argv[])
 {
-	
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+
+    ZeroMemory(&si, sizeof(STARTUPINFO));
+    ZeroMemory(&pi, sizeof(PROCESS_INFORMATION));
+
+    si.cb = sizeof(STARTUPINFO);
+
+    char* DirPath = new char[MAX_PATH];
+    char* DLLPath = new char[MAX_PATH]; //testdll.dll
+    char* DetourPath = new char[MAX_PATH]; //detoured.dll
+
+    GetCurrentDirectory(MAX_PATH, (LPWSTR)DirPath);
+
+    HINSTANCE hMyDll;
+    if ((hMyDll = LoadLibrary(dllName)) == NULL)
+    {
+        return 1;
+    }
+
+    DetourCreateProcessWithDll(L"C:\\windows\\notepad.exe", NULL, NULL, NULL, false, CREATE_DEFAULT_ERROR_MODE, NULL, NULL, &si, &pi, (LPCSTR)dllName, NULL);
+
+    delete[] DirPath;
+    delete[] DLLPath;
+    delete[] DetourPath;
+    return 0;
 }
